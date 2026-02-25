7319a9e7fb58303c24795d131275f0a195d1c1f2
salik nisar <beighsalik51@gmail.com>
2026-02-25 08:41:08 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import CarScrollSection from "./components/CarScrollSection";
+  import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div>
+      <Analytics />
       <CarScrollSection />
     </div>
   );
